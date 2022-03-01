99d1befd33be55c14304f45c08b23ca5aef8a99c
mdjahid66 <mohammadjahid0007@gmail.com>
2022-03-01 20:57:55 +0600

    others feautures added

--- mobile-world.js
+++ mobile-world.js
@@ -21,9 +21,8 @@ const displayPhones = (phones) => {
             <div class="card rounded-3 align-items-center shadow-lg bg-body rounded border-0 pt-3">
                 <img src="${phone.image}" class="card-img-top w-75" alt="...">
                 <div class="card-body">
-                    <h6 class="card-title">${phone.phone_name}</h6>
-                    <p class="card-text">
-                    Brand: ${phone.brand}</p>
+                    <h4 class="card-title">${phone.phone_name}</h4>
+                    <h6 class="fs-5 card-text"><span class="fw-bolder ">Brand:</span>${phone.brand}</h6>
                     <button onclick="loadPhoneDetails('${phone.slug}')" class="card-btn btn btn-success px-1 fs-6"><span class="btn-text">Show Details</span></button>
                 </div>
             </div>
@@ -46,12 +45,22 @@ const displayDetails = (phoneId) => {
                 <img src="${phoneId.image}" class="card-img-top w-75" alt="...">
                 <div class="card-body gy-3">
                     <h6><span class="fw-bold">Name:</span> ${phoneId.name}</h6>
-                    <h6><span class="fw-bold">Realese Date:</span> ${phoneId.releaseDate}</h6>
+                    <h6><span class="fw-bold">Realese Date:</span> ${phoneId.releaseDate === '' ? 'unavailable' : `${phoneId.releaseDate}`}</h6>
                     <h6><span class="fw-bold">Brand:</span> ${phoneId.brand}</h6>
                     <h6><span class="fw-bold">Storage:</span> ${phoneId.mainFeatures.storage}</h6>
                     <h6><span class="fw-bold">Display Size:</span> ${phoneId.mainFeatures.displaySize}</h6>
                     <h6><span class="fw-bold">Chipset:</span> ${phoneId.mainFeatures.chipSet}</h6>
                     <h6><span class="fw-bold">Sensor:</span> ${arrayToString(phoneId.mainFeatures.sensors)}</h6>
+                    ${phoneId.others ? `
+                    <h5 class="fw-bold my-3">More Specifications</h5>
+                    <ul>
+                        <li><span class="fw-bolder">Bluetooth:</span> ${phoneId.others.Bluetooth}</li>
+                        <li><span class="fw-bolder">GPS:</span> ${phoneId.others.GPS}</li>
+                        <li><span class="fw-bolder">NFC:</span> ${phoneId.others.NFC}</li>
+                        <li><span class="fw-bolder">Radio:</span> ${phoneId.others.Radio}</li>
+                        <li><span class="fw-bolder">USB:</span> ${phoneId.others.USB}</li>
+                        <li><span class="fw-bolder">WLAN:</span> ${phoneId.others.WLAN}</li>
+                    </ul>` : ''}
                 </div>
         </div>
     `
